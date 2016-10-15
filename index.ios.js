b2eabb0ac32ae3eb6c363aa92196d8bf654c40d0
Pratik Patel <ipratikpatel@gmail.com>
2016-10-15 16:35:16 -0500

    Adding Property Finder project built using ReactJS native.

---
+++ index.ios.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+'use strict';
+var React = require('react');
+var ReactNative = require('react-native');
+var SearchPage = require('./SearchPage');
+
+var styles = ReactNative.StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+
+
+class PropertyFinderApp extends React.Component {
+  render() {
+    return (
+      <ReactNative.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+ReactNative.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
